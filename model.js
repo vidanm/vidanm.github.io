726804f78abc1b490d7de297b284b71c61aa6829
vidanm <vidan.tauriff@gmail.com>
2021-06-15 16:28:27 +0200

    feature: fade in animation

--- model.js
+++ model.js
@@ -8,4 +8,24 @@ window.onload = function(){
 function displayBig(){
 	var bigImage = document.getElementById("fullImage");
 	bigImage.src = this.src;
+	animateCSS(bigImage,'fadeInLeft');
+
 }
+
+const animateCSS = (element,animation, prefix = 'animate__') =>
+	  // We create a Promise and return it
+	new Promise((resolve, reject) => {
+	const animationName = `${prefix}${animation}`;
+	const node = element;
+	
+	node.classList.add(`${prefix}animated`, animationName);
+
+	// When the animation ends, we clean the classes and resolve the Promise
+	function handleAnimationEnd(event) {
+		event.stopPropagation();
+		node.classList.remove(`${prefix}animated`, animationName);
+		resolve('Animation ended');
+	}
+	
+	node.addEventListener('animationend', handleAnimationEnd, {once: true});
+});
