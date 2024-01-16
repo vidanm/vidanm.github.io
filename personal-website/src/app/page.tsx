818b89c529b1098369cc0a9118a9560f18f10135
vidanmurati <vidan.sloops@gmail.com>
2024-01-16 21:52:14 +0100

    add major features

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -1,56 +1,106 @@
 "use client";
 import { motion } from 'framer-motion';
+import { readdirSync } from 'fs';
+import React from 'react';
 
-function SmoothTransition({children}:Readonly<{children:React.ReactNode}>){
+let goTo = ({ url }: Readonly<{ url: string }>) => {
+  location.href = url;
+}
+
+function SquareImage({ src }: Readonly<{ src: string }>) {
   return (
-    <motion.div initial={{opacity:0,x:-100}} animate={{opacity:1,x:0}} transition={{type:"spring",duration:1}}>
+    <motion.img initial={{ x: 500, zIndex: -10 }} viewport={{ once: true }} whileInView={{ x: 0 }} transition={{ type: "spring", duration: 2 }} whileHover={{ scale: 1.3 }} src={src} className='aspect-square rounded-lg' />
+  )
+}
+
+function SmoothTransition({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <motion.div initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x: 0 }} transition={{ type: "spring", duration: 1 }}>
       {children}
     </motion.div>
   )
 }
 
-function MyButton({ name }: Readonly<{ name: string }>) {
-  return (<motion.button whileHover={{scale:1.1}} style={{ width: 200 }} className="text-orange-300 hover:bg-orange-300 hover:text-white border border-orange-300 p-2 my-2 rounded-lg" type="button">{name}</motion.button>)
+function MyButton({ name,src }: Readonly<{ name: string,src:string }>) {
+  return (<motion.button whileHover={{ scale: 1.1 }} onClick={(e) => { location.href = src }} style={{ width: 200 }} className="text-orange-300 hover:bg-orange-300 hover:text-white border border-orange-300 p-2 my-2 rounded-lg" type="button">{name}</motion.button>)
 }
 
 function Resume() {
   return (
-  <div>
-    <h3 >Vidan MURATI</h3>
-    <h1>Développeur Junior </h1>
-    <h5><span className="text-orange-300">Compétences</span> en C++ / Programmation orientée objet / OpenGL </h5>
-    <p className="my-2">
-      Jeune diplômé d'un Master Informatique Sciences de l'image,
-      passionné d'algorithmie, des technologies 3D et de vision par ordinateur.
-    </p>
-  </div>);
+    <div>
+      <h3 >Vidan MURATI</h3>
+      <h1 className="font-bold">Développeur Junior </h1>
+      <h5 className="font-bold"><span className="text-orange-300">Compétences</span> en C++ / Programmation orientée objet / OpenGL </h5>
+      <p className="my-2">
+        Jeune diplômé d'un Master Informatique Sciences de l'image,
+        passionné d'algorithmie, des technologies 3D et de vision par ordinateur.
+      </p>
+      <MyButton name={"Découvrir mon CV"} src={"CV/CV.pdf"}></MyButton>
+    </div>);
 }
 
 function RoundedPicture() {
-  return (<motion.img initial={{y:-100,rotate:180,borderRadius:50}} animate={{y:0,rotate:0,borderRadius:100}} alt="Vidan MURATI" style={{ width: "200px" }} className="rounded-full shadow-lg" src="photo_profil.jpg" />)
+  return (<motion.img initial={{ opacity: 0, width: 200 }} animate={{ opacity: 1, width: 200 }} alt="Vidan MURATI" className="" src="photo_profil_3.jpg" />)
 }
 
-function Logo({source} : Readonly<{source:string}>) {
-  return (<img style={{width:50}} src={source} alt="linkedin"/>)
+function Logo({ source, url }: Readonly<{ source: string, url: string }>) {
+  return (<motion.img whileHover={{ scale: 1.1 }} style={{ width: 50 }} src={source} onClick={(e) => { location.href = url }} alt="linkedin" />)
 }
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen items-center justify-between p-24">
-      <div className="flex flex-col">
-        <div className="flex-row flex">
-          <div className="flex flex-col p-4 m-4">
-            <SmoothTransition>
-              <Resume/>
-              <MyButton name={"En savoir plus"} />
-            </SmoothTransition>
+    <main>
+      <div className="flex min-h-screen items-center justify-between p-48">
+        <SmoothTransition>
+          <div className="rounded flex flex-col bg-white shadow-lg border-orange-100 border-4" style={{ opacity: 0.9 }}>
+            <div className="flex-row flex ">
+              <div className="flex flex-col p-4 m-4">
+                <Resume />
+              </div>
+              <div className="flex my-auto flex-col p-4 m-4">
+                <RoundedPicture />
+              </div>
+            </div>
           </div>
-          <div className="flex my-auto flex-col p-4 m-4">
-            <RoundedPicture />
+        </SmoothTransition>
+        <div className="flex-col justify-center m-4 items-center flex">
+          <div className='m-1'>
+            <Logo source={"linkedin.png"} url={"https://www.linkedin.com/in/vidan-murati-ab02b91b3/"} />
+          </div>
+          <div className='m-1'>
+            <Logo source={"Arobase.png"} url={"mailto:vidanmurati@hotmail.fr"} />
+          </div>
+          <div className='m-1'>
+            <Logo source={"Instagram.png"} url={"https://www.instagram.com/vidanm_/"}/>
           </div>
         </div>
-        <div className="flex-row justify-center m-4 items-center flex">
-          <Logo source={"linkedin.png"}/>
+      </div>
+      <div className='rounded-lg m-24 '>
+        <div className="flex-row flex justify-center ">
+          <div className="flex flex-col justify-center items-center basis-2/5 p-4 m-4">
+            <div className="grid grid-cols-4 gap-1">
+              <SquareImage src={"3D/Church2.jpg"} />
+              <SquareImage src={"3D/cofee.png"} />
+              <SquareImage src={"3D/Ecrire.jpg"} />
+              <SquareImage src={"3D/Origami.jpg"} />
+              <SquareImage src={"3D/Photo.jpg"} />
+              <SquareImage src={"3D/porcorossi.png"} />
+              <SquareImage src={"3D/raquette.png"} />
+              <SquareImage src={"3D/telephone.png"} />
+              <SquareImage src={"3D/TV.png"} />
+              <SquareImage src={"3D/Velo.jpg"} />
+              <SquareImage src={"3D/tulp.png"} />
+              <SquareImage src={"Dessins/fusain_1.png"} />
+              <SquareImage src={"Dessins/plante.png"} />
+              <SquareImage src={"Dessins/portrait.png"} />
+              <SquareImage src={"Dessins/noel.png"} />
+              <SquareImage src={"3D/Church.png"} />
+
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center p-4 m-4 basis-2/5">
+            <SquareImage src={"3D/church.png"} />
+          </div>
         </div>
       </div>
     </main>
