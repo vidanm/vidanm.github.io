16b1ce2ea779be05aa5a9112a1ffc14e53b00218
vidanmurati <vidan.sloops@gmail.com>
2024-01-17 11:32:21 +0100

    nextjs config fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'build',
+    // distDir: 'build',
     webpack: (config) => {
         config.resolve.fallback = { fs: false,v8:false,module:false,perf_hooks:false };
         return config;  
